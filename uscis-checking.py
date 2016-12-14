7d2dcb467451de10528d67fb074a105d8ea3201f
wmcalyj <mengchaowang@gmail.com>
2016-12-13 21:19:41 -0600

    Fix python script typo

---
+++ uscis-checking.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import urllib
+import urllib2
+import datetime
+import time
+from bs4 import BeautifulSoup
+
+
+def readReceiptNumbers(filename):
+    time = datetime.datetime.now()
+    timeStr = time.strftime('%Y-%b-%d %H:%M:%S')
+    f = open(filename, 'r')
+    noNumber = True
+    for line in f:
+        if not line.strip():
+            continue
+        number = line.strip()[:13]
+        if number[0] != '#':
+            noNumber = False
+            try:
+                status = getStatus(number)
+                
+                print timeStr + ' -- receipt number: [' + number + '], case status: [' + status + ']'
+            except AttributeError:
+                print 'Invalid receipt number: [' + number + '], please check your receipt number and try again'
+    if noNumber:
+        print 'No receipt number is found in ' + filename + ', please put your receipt number there.'
+
+def getStatus(receiptNumber):
+    url = 'https://egov.uscis.gov/casestatus/mycasestatus.do'
+    # changeLocale=&appReceiptNum=YSC1790007419&initCaseSearch=CHECK+STATUS
+    values = {'changeLocale' : '',
+              'appReceiptNum' : receiptNumber,
+              'initCaseSearch' : 'CHECK STATUS' }
+    data = urllib.urlencode(values)
+    req = urllib2.Request(url, data)
+    response = urllib2.urlopen(req)
+    the_page = response.read()
+    soup = BeautifulSoup(the_page, 'html.parser')
+    statusTag = soup.find('div', {'class':'current-status-sec'})
+    contents = statusTag.text.split("\n")
+    content = contents[2].strip()
+    return content
+
+readReceiptNumbers('number.txt')
